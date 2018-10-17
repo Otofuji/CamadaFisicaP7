bc85d5aa4ecc335b429a4943186e5eb50498c2f5
Eric Otofuji Abrantes <otofuji@MacBook-Pro-de-Eric.local>
2018-10-17 17:08:25 -0300

    DTMF

--- DTMF.py
+++ DTMF.py
@@ -13,22 +13,22 @@
 ####################################################
 # OBJETIVOS:
 
-# Transmitir símbolos das teclas do telefone
-# Receber símbolos das teclas do telefone
-# Usuário deve teclar valor a ser transmitido ao rodar a transmissão
+# Transmitir símbolos das keys do telefone
+# Receber símbolos das keys do telefone
+# Usuário deve keyr valor a ser transmitido ao rodar a transmissão
 # Transmissão deve ser realizada em alguns segundos (parâmetro do código)
 # Aplicação deve investigar o sinal pelos símbolos a cada 1 segundo
 # Plotar gráfico do sinal recebido e dos harmônicos (Fourier)
-# Print da tecla pressionada
+# Print da key pressionada
 
 ####################################################
 # AVALIAÇÃO:
 
-# Sinal gerado com sucesso e reproduzido no alto-falante ao pressionar tecla.
+# Sinal gerado com sucesso e reproduzido no alto-falante ao pressionar key.
 # Gráfico de sinal emitido.
 # Recepção e Fourier do sinal recebido com identificação dos picos.
 # Gráfico do sinal recebido e gráfico dos harmônicos
-# Identificação correta da tecla pressionada pelo usuário.
+# Identificação correta da key pressionada pelo usuário.
 
 ####################################################
 
@@ -39,6 +39,7 @@ import matplotlib.pyplot as plt
 import wave
 import time
 import pickle
+import keyboard
 import peakutils
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
@@ -48,13 +49,13 @@ from scipy import signal as window
 ####################################################
 #SignalClass
 
-def generateSin(self, freq, amplitude, time, fs):
+def generateSin(freq, amplitude, time, fs):
     n = time*fs
     x = np.linspace(0.0, time, n)
     s = amplitude*np.sin(freq*x*2*np.pi)
     return (x, s)
 
-def calcFFT(self, signal, fs):
+def calcFFT(signal, fs):
     N  = len(signal)
     W = window.hamming(N)
     T  = 1/fs
@@ -62,7 +63,7 @@ def calcFFT(self, signal, fs):
     yf = fft(signal*W)
     return(xf, np.abs(yf[0:N//2]))
 
-def plotFFT(self, signal, fs):
+def plotFFT(signal, fs):
     x,y = self.calcFFT(signal, fs)
     plt.figure()
     plt.plot(x, np.abs(y))
@@ -72,10 +73,10 @@ def plotFFT(self, signal, fs):
 
 
 
-def signalFrequency(tecla):
+def signalFrequency(key):
 
 	####################################################
-	# Define as frequências das teclas
+	# Define as frequências das keys
 	####################################################
 	# Padrão DTMF Bell Labs (Hz)
 
@@ -90,44 +91,167 @@ def signalFrequency(tecla):
 	#9: 1477 & 852
 	#*: 1209 & 941
 	#0: 1336 & 941
-	##: 1477 & 941
+	##: 1477 & 941 
 
 	####################################################
 	
 
 	return {
 
-		"1": (generateSin(self,1209,21,1,44100) + generateSin(self,697,21,1,44100)),
-		"2": (generateSin(self,1336,21,1,44100) + generateSin(self,697,21,1,44100)),
-		"3": (generateSin(self,1477,21,1,44100) + generateSin(self,697,21,1,44100)),
-		"4": (generateSin(self,1209,21,1,44100) + generateSin(self,770,21,1,44100)),
-		"5": (generateSin(self,1336,21,1,44100) + generateSin(self,770,21,1,44100)),
-		"6": (generateSin(self,1477,21,1,44100) + generateSin(self,770,21,1,44100)),
-		"7": (generateSin(self,1209,21,1,44100) + generateSin(self,852,21,1,44100)),
-		"8": (generateSin(self,1336,21,1,44100) + generateSin(self,852,21,1,44100)),
-		"9": (generateSin(self,1477,21,1,44100) + generateSin(self,852,21,1,44100)),
-		"0": (generateSin(self,1336,21,1,44100) + generateSin(self,941,21,1,44100)),
-		"*": (generateSin(self,1209,21,1,44100) + generateSin(self,941,21,1,44100)),
-		"#": (generateSin(self,1477,21,1,44100) + generateSin(self,941,21,1,44100))
+		"1": (generateSin(1209,21,1,44100) + generateSin(697,21,1,44100)),
+		"2": (generateSin(1336,21,1,44100) + generateSin(697,21,1,44100)),
+		"3": (generateSin(1477,21,1,44100) + generateSin(697,21,1,44100)),
+		"4": (generateSin(1209,21,1,44100) + generateSin(770,21,1,44100)),
+		"5": (generateSin(1336,21,1,44100) + generateSin(770,21,1,44100)),
+		"6": (generateSin(1477,21,1,44100) + generateSin(770,21,1,44100)),
+		"7": (generateSin(1209,21,1,44100) + generateSin(852,21,1,44100)),
+		"8": (generateSin(1336,21,1,44100) + generateSin(852,21,1,44100)),
+		"9": (generateSin(1477,21,1,44100) + generateSin(852,21,1,44100)),
+		"0": (generateSin(1336,21,1,44100) + generateSin(941,21,1,44100)),
+		"*": (generateSin(1209,21,1,44100) + generateSin(941,21,1,44100)),
+		"#": (generateSin(1477,21,1,44100) + generateSin(941,21,1,44100)),
 
-	}.get(tecla, None)
-
-signalFrequency(5)
+	}.get(key, None);
 
 
+def testSignalSound():
+	sd.play(signalFrequency(str(1))[1] + signalFrequency(str(1))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(2))[1] + signalFrequency(str(2))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(3))[1] + signalFrequency(str(3))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(4))[1] + signalFrequency(str(4))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(5))[1] + signalFrequency(str(5))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(6))[1] + signalFrequency(str(6))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(7))[1] + signalFrequency(str(7))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(8))[1] + signalFrequency(str(8))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(9))[1] + signalFrequency(str(9))[3], 44100); sd.wait();
+	sd.play(signalFrequency(str(0))[1] + signalFrequency(str(0))[3], 44100); sd.wait();
+	sd.play(signalFrequency(   "*")[1] + signalFrequency(   "*")[3], 44100); sd.wait();
+	sd.play(signalFrequency(   "#")[1] + signalFrequency(   "#")[3], 44100); sd.wait();
+
+#testSignalSound();
+
+def getKey():
+	while True:
+	    try:
+	        if   keyboard.is_pressed('1'):
+	            return "1"
+	            break
+	        elif keyboard.is_pressed('1'):
+	            return "1"
+	            break
+	        else:
+	            pass
+	    except:
+	        break
+
+def testKey():
+	key = input("Insira um número para transmitir: ");
+	print(str(key));
+
+#testKey();
 
 
 
 
+def play():
+	####################################################
+	# OBJETIVOS:
+
+	# Transmitir símbolos das keys do telefone
+	# Print da key pressionada
+
+	####################################################
+	# AVALIAÇÃO:
+
+	# Sinal gerado com sucesso e reproduzido no alto-falante ao pressionar key.
+
+	####################################################
+	pass
 
 
+def listen():
+	####################################################
+	# OBJETIVOS:
 
+	# Receber símbolos das keys do telefone
+	# Transmissão deve ser realizada em alguns segundos (parâmetro do código)
+	# Aplicação deve investigar o sinal pelos símbolos a cada 1 segundo
 
+	####################################################
+	# AVALIAÇÃO:
 
+	####################################################
+	pass
 
+def getSignal():
+	####################################################
+	# OBJETIVOS:
 
+	# Aplicação deve investigar o sinal pelos símbolos a cada 1 segundo
+	# Plotar gráfico do sinal recebido e dos harmônicos (Fourier)
 
+	####################################################
+	# AVALIAÇÃO:
 
+	# Recepção e Fourier do sinal recebido com identificação dos picos.
+	# Identificação correta da key pressionada pelo usuário.
+
+	####################################################
+	pass
+
+def plotGenerateSignal():
+	####################################################
+	# OBJETIVOS:
+
+	####################################################
+	# AVALIAÇÃO:
+
+	# Gráfico de sinal emitido.
+
+	####################################################
+	pass
+
+def plotReceivedSignal():
+	####################################################
+	# OBJETIVOS:
+
+	# Plotar gráfico do sinal recebido e dos harmônicos (Fourier)
+	# Print da key pressionada
+
+	####################################################
+	# AVALIAÇÃO:
+
+	# Gráfico do sinal recebido e gráfico dos harmônicos
+
+	####################################################
+	pass
+
+def discoverReceivedSignal():
+	####################################################
+	# OBJETIVOS:
+
+	# Print da key pressionada
+
+	####################################################
+	# AVALIAÇÃO:
+
+	# Identificação correta da key pressionada pelo usuário.
+
+	####################################################
+	pass
+	
+
+def mainGenerate():
+	while True:
+		key = getKey()
+		signalFrequency(key)
+
+#mainGenerate():
+
+def mainReceive():
+	pass
+
+#mainReceive()
 
 
 
